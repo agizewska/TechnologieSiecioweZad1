3b08ce70563ab9d33c20029d3d1d02fceef0b3d9
agizewska <ola.gizewska@gmail.com>
2018-06-08 17:03:08 +0200

    .

--- src/pwr/businesslogic/Calculator.java
+++ src/pwr/businesslogic/Calculator.java
@@ -8,19 +8,15 @@ public class Calculator {
 
     public static double calculateSum(List<Product> products){
         double sum = 0;
-
             for (int i = 0; i < products.size(); i++) {
                 sum += products.get(i).getPrice();
         }
-
         return sum;
     }
 
     public static double calculateAVG(List <Product> products){
         double avg = 0;
-        if (!products.isEmpty()){
-            avg = calculateSum(products)/products.size();
-        }
+        avg = calculateSum(products)/products.size();
         return avg;
     }
 }
